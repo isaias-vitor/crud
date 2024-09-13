6ff48ba9694fdab5d58532b3d139dbe110fcb89d
Isaias Vitor Silva Oliveira <isaiasvitorslva@gmail.com>
2024-09-13 10:32:30 -0300

    Ajuste para igualar aos arquivo originais

--- src/main/java/com/mycompany/crud/Crud.java
+++ src/main/java/com/mycompany/crud/Crud.java
@@ -14,5 +14,10 @@ public class Crud {
 
     public static void main(String[] args) {
         
+        UserDao userDao = new UserDao();
+
+        User aZ = new User(1L, "bb Zaira", "a.zaira@mail.com", "123", LocalDate.now(), true);
+        userDao.saveOrUpdate(aZ);
+        
     }
 }
